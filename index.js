4e13b81ae37f0d96ad58e338cd8ed48abd84b5c8
Manister <harry@toolshed.design>
2020-06-11 18:01:32 +0100

    build client side script

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const fs = require('fs');
 const glob = require("glob")
 
+const minify = require('@node-minify/core');
+const babelMinify = require('@node-minify/babel-minify');
+
 const handleSrcs = require('./lib/handleSrcs')
 const handleSrcsets = require('./lib/handleSrcsets')
 
-const lazyLoadScript = fs.readFileSync('./client/dist/lazyLoad.js')
-const scriptToInject = `<script type="text/javascript">${lazyLoadScript}</script>`
+const scriptToInjectPromise = async () =>
+  `<script type="text/javascript">${await minify({
+    compressor: babelMinify,
+    input: 'client/src/lazyLoad.js',
+    output: 'client/dist/lazyLoad.js',
+    sync: true,
+    options: {
+      presets: ['babel-preset-env']
+    }
+  })}</script > `
 
 module.exports = {
   onSuccess: async ({ inputs, constants, utils }) => {
@@ -13,11 +24,11 @@ module.exports = {
     const imageConfig = { paletteSize, dir: constants.PUBLISH_DIR }
     const updateSrcs = handleSrcs(imageConfig)
     const updateSrcsets = handleSrcsets(imageConfig)
+    const scriptToInject = await scriptToInjectPromise();
     try {
       const files = glob.sync(`${constants.PUBLISH_DIR}/**/*.html`, {
-        ignore: exclude.map(exclusion => `${constants.PUBLISH_DIR}/${exclusion}`)
+        ignore: exclude.map(exclusion => `${constants.PUBLISH_DIR} /${exclusion}`)
       })
-      console.log(files)
       const updateFilesPromises = files.map(async file => {
         const fileData = fs.readFileSync(file)
         const fileDataString = fileData.toString()
