96ac5a4a87696f610ab53568f8650c59b374ed9e
Manister <harry@toolshed.design>
2020-06-13 10:55:47 +0100

    delete dist files, add npm ignore

--- build-client-js.js
+++ build-client-js.js
@@ -6,12 +6,17 @@ const buildClientJs = async () => {
   const script = await minify({
     compressor: babelMinify,
     input: 'client/src/lazyLoad.js',
-    output: 'client/dist/lazyLoad.js',
+    output: 'client/lazyLoad.js',
     options: {
       presets: ['babel-preset-env']
     }
   });
   const jsonified = JSON.stringify({ script });
+  try {
+    fs.unlinkSync('client/lazyLoad.js')
+  } catch (err) {
+    console.error(err)
+  }
   fs.writeFileSync('client/lazyLoadClientScipt.json', jsonified, 'utf-8')
 }
 
