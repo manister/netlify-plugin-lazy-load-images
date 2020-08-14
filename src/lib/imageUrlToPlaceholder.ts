9aece35814b4904d46375c50819108763ee26f03
Harry <24208175+manister@users.noreply.github.com>
2020-08-14 19:37:35 +0100

    logs

--- src/lib/imageUrlToPlaceholder.ts
+++ src/lib/imageUrlToPlaceholder.ts
@@ -3,6 +3,7 @@ import getPlaceHolder from './getPlaceHolder'
 const imageUrlToPlaceholder = async (imageUrl: string) => {
   try {
     const placeholder = await getPlaceHolder(imageUrl);
+    console.log("\x1b[32m", `Placeholder generated for ${imageUrl}`, "\x1b[32m",)
     return placeholder
   } catch (e) {
     console.warn("\x1b[31m", `Couldn't get placeholder for ${imageUrl}, ${e}`, "\x1b[32m",)
